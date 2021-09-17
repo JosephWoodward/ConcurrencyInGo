cd4527e7a98a4482642a6b9ab412b61febd40da5
Joseph Woodward <josephwoodward@xeuse.com>
2021-09-17 17:54:01 +0100

    Update 2_fanout_example.go

--- concurrency_building_blocks/6_orchannel/2_fanout_example.go
+++ concurrency_building_blocks/6_orchannel/2_fanout_example.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type Result struct {
@@ -17,16 +18,23 @@ func main() {
 	urls := []string{"https://stackoverflow.com", "https://google.co.uk", "https://bbc.co.uk"}
 
 	done := make(chan struct{})
+	resCount := 0
 
 	for result := range checkStatus(done, urls...) {
+		resCount++
 		if result.err != nil {
 			fmt.Printf("Error: %v", result.err)
 			continue
 		}
 		fmt.Printf("Response: %v = %v\n", result.url, result.response.Status)
+		if resCount == len(urls) {
+			done <- struct{}{}
+			break
+		}
 	}
 
-	<-done
+	// done <- struct{}{}
+	// Can you use context instead of done? Can you share it between channels
 
 	fmt.Printf("Finished!")
 }
@@ -36,14 +44,16 @@ func checkStatus(done chan struct{}, urls ...string) <-chan Result {
 
 	for _, v := range urls {
 		go func(done <-chan struct{}, url string) {
+			// Heavy work is already done here, it should be moved into case after done.
+			time.Sleep(2 * time.Second)
 			res, err := http.Get(url)
 			r := Result{response: res, err: err, url: url}
+
 			select {
 			case <-done:
 				return
 			case results <- r:
 			}
-			return
 		}(done, v)
 	}
 
